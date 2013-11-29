4f63fe34d395aac9b657959e8b28bd9bd31f9cbb
g21-ck <kiet5629@hotmail.com>
2013-11-28 21:22:43 -0800

    Final version of part A

--- ClientExample/client.c
+++ ClientExample/client.c
@@ -7,6 +7,7 @@
 
 int main (int argc, char *argv [])
 {
+	//declare all variables
 	WSADATA wsa;
 	SOCKET sck,retSocket;
 	int c;
@@ -16,33 +17,35 @@ int main (int argc, char *argv [])
 	char msg [100];
 
 
-
+	//set up Winsock
 	if (WSAStartup (MAKEWORD (2,2), &wsa) == INVALID_SOCKET)
 		printf ("WSA error\n");
-	else if ((sck = socket (AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+	else if ((sck = socket (AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)//create socket
 		printf ("Socket error\n");
 	else{
 		ip_info.sin_addr.s_addr = inet_addr("127.0.0.1");
 		ip_info.sin_family = AF_INET;
 		ip_info.sin_port = htons (8888);
 	}
-
+	//connect to server
 	if (connect (sck, (struct sockaddr *)&ip_info, sizeof (ip_info)) < 0)
 		printf ("\nNot connected\n");
 	else
 		printf ("\nSession is up\n");
 
-	//retSocket = accept(sck , (struct sockaddr *)&server, &c);
+	//loop forever
 	while(1){
 		printf("\n");
 		printf ("Send: ");
-		gets(msg);
+		gets(msg);//ask the user to input message
 
+		//send message via Socket
 		if(send(sck , msg , strlen(msg) , 0) == SOCKET_ERROR)
 			puts("fail");
 		else
 			printf("Message Successfully sent to Server\n");
-		
+
+		//the size of message from server and get the message from server
 		recv_size = recv(sck, server_reply, 2000, 0);
 		server_reply[recv_size] = '\0';
 		if ( recv_size== SOCKET_ERROR) {
@@ -53,20 +56,9 @@ int main (int argc, char *argv [])
 			puts(server_reply);
 		}
 	}
-	/*while (recv_size = recv(sck, server_reply, 2000, 0)) {
-		if (recv_size == SOCKET_ERROR) {
-			puts("recv failed");
-		} else { 
-			server_reply[recv_size] = '\0';
-			puts(server_reply);
-		}
-	}*/
-
-
-
-
+	
 	getch ();
-	closesocket(sck);
+	closesocket(sck);//cloe socket
     return 0;
 }
 
--- ServerExample/server.c
+++ ServerExample/server.c
@@ -5,7 +5,7 @@
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
 int main(int argc , char *argv[])
 {
-
+	//declare all variables
 	WSADATA wsa ;
 	SOCKET s , new_socket;
 	struct sockaddr_in server , client;
@@ -14,6 +14,7 @@ int main(int argc , char *argv[])
 	int recv_size;
 	char client_reply[2000];
 
+	//initialising Winsock
 	printf("\nInitialising Winsock...");
 	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
 	{
@@ -23,18 +24,19 @@ int main(int argc , char *argv[])
 
 	printf("Initialised.\n");
 
-
+	//create socket
 	if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
 	{
 		printf("Could not create socket : %d" , WSAGetLastError());
 	}
 	printf("Socket created.\n");
+	//set socket's information
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = inet_addr ("127.0.0.1");
 	server.sin_port = htons( 8888 );
 
 
-
+	//bind socket
 	if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
 	{
 		printf("Bind failed with error code : %d" , WSAGetLastError());
@@ -42,7 +44,7 @@ int main(int argc , char *argv[])
 	printf("Bind done...");
 	listen(s , 3);
 
-	
+	//connect to client
 	puts("Waiting for incoming connections...");
 
 	c = sizeof(struct sockaddr_in);
@@ -53,25 +55,21 @@ int main(int argc , char *argv[])
 	}
 
 	puts("Connection accepted");
-	/*while(1){
-		printf ("Send: ");
-		gets(msg);
 
-		send(s , msg , strlen(msg) , 0);
-	}*/
-
+	//loop forever
 	while (1) {
 		printf("\n");
+		//get the size of received message
 		recv_size = recv(new_socket, client_reply, 2000, 0);
 		if (recv_size == SOCKET_ERROR) {
 			puts("recv failed");
 		} else { 
 			client_reply[recv_size] = '\0';
 			printf("Receive: ");
-			puts(client_reply);
+			puts(client_reply);//print out the message from client
 		}
 		printf("Successfully received message\n");
-
+		//send the message back to client
 		if(send(new_socket , client_reply , strlen(client_reply) , 0) == SOCKET_ERROR)
 			puts("fail to reply the client.\n");
 		else
@@ -79,6 +77,7 @@ int main(int argc , char *argv[])
 
 
 	}
+	//close socket
 	closesocket(s);
 	closesocket(new_socket);
 	return 0;
